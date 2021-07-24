43c5d3eb2f841317bd0dadfc6a1052865051c71c
Melquizedeque S. Lobo <melquilobo@hotmail.com>
2021-07-24 09:18:00 -0300

    Implementação da classe Frame
    Modificação do package.json

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ Esse framework só foi posível graças ao NodeGui
 Melquizedeque S. Lobo
 */
 const Window = require("./components/Window")
+const Frame = require("./components/Frame")
 
+/* WINDOW  */
 module.exports.window = (...props) => {
 
     let properties = null;
@@ -14,6 +16,26 @@ module.exports.window = (...props) => {
     })
 
    return new Window()
+        .setProperties(properties)
+        .addChildren(children)
+        .show()
+
+
+
+
+}
+
+/* FRAME  */
+
+module.exports.frame = (...props) => {
+
+    let properties = null;
+    let children = null;
+    props.forEach((prop) => {
+        typeof prop == "object" ? Array.isArray(prop) ? children = prop : properties = prop : typeof props == "string" ? children = prop : null;
+    })
+
+   return new frame()
         .setProperties(properties)
         .addChildren(children)
         .show()
